10253e58cf976460f845010e76b5ae75c3ab2083
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-04-29 14:09:20 +0800

    Confirm: add gasError prop to CustomGas

--- app/components/Views/SendFlow/Confirm/index.js
+++ app/components/Views/SendFlow/Confirm/index.js
@@ -418,7 +418,7 @@ class Confirm extends PureComponent {
 	};
 
 	renderCustomGasModal = () => {
-		const { customGasModalVisible, currentCustomGasSelected } = this.state;
+		const { customGasModalVisible, currentCustomGasSelected, gasError } = this.state;
 		const { gas, gasPrice } = this.props.transactionState.transaction;
 		return (
 			<Modal
@@ -440,6 +440,7 @@ class Confirm extends PureComponent {
 					handleGasFeeSelection={this.handleGasFeeSelection}
 					gas={gas}
 					gasPrice={gasPrice}
+					gasError={gasError}
 					toggleCustomGasModal={this.toggleCustomGasModal}
 					handleSetGasFee={this.handleSetGasFee}
 				/>
